34c69b4baf06e093e34ef33c620f43d2991bd821
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-12-03 20:57:36 +0100

    feat: add commander

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import { Command } from "commander";
+import { ping } from "./commands/ping";
+
+async function main() {
+	const program = new Command()
+		.name("Svelte-CLI")
+		.description("Svelte-CLI is a command line tool for Svelte")
+		.version("0.0.1", "-v, --version", "output the current version");
+
+	program.addCommand(ping);
+	program.parse();
+}
+
+main();
